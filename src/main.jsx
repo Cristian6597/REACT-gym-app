18fbe891bacdfc370cb0a8a4ccbf8b1e2928af03
Cristian6597 <cristianercolani6597@gmail.com>
2025-05-21 15:46:31 +0200

    implementato react router dom e pagina login

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,22 @@ import "./index.css";
 import { SidebarProvider } from "./components/ui/sidebar.jsx";
 import { ThemeProvider } from "./components/theme-provider.jsx";
 
+// 👉 Importa BrowserRouter
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage.jsx";
+import { LoginForm } from "./components/login-form.jsx";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SidebarProvider>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <App />
+        {/* 👉 Avvolgi tutto con BrowserRouter */}
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </BrowserRouter>
       </ThemeProvider>
     </SidebarProvider>
   </React.StrictMode>
