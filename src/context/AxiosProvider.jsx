47e9002eec0468f207a1ed83019a0514987bfff0
Cristian6597 <cristianercolani6597@gmail.com>
2025-06-19 23:32:43 +0200

    aggiornato url

--- src/context/AxiosProvider.jsx
+++ src/context/AxiosProvider.jsx
@@ -4,7 +4,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 const axiosContext = createContext();
 
 const myaxios = axios.create({
-  baseURL: import.meta.env.RENDER_BASE_URL,
+  baseURL: import.meta.env.VITE_RENDER_BASE_URL,
   headers: {
     Accept: "application/json",
   },
